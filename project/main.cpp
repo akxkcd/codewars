96e258d0648120204cdb88789032fe4eadae4c09
akxkcd <anurag.iitkgp@gmail.com>
2016-06-18 17:41:20 -0700

    added support to take any file.

--- project/main.cpp
+++ project/main.cpp
@@ -4,9 +4,16 @@
 using namespace std;
 ///using std::pair;
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
 	
-   auto points = IO_Points<float>::readCSV(string("data/sample_data.csv"));
+   vector<Point<float> > points;
+   cout << argc << endl;
+   if (argc == 2) {
+      cout << argv[0] << " " << argv[1] << endl;
+      points = IO_Points<float>::readCSV(string(argv[1]));
+   }else { 
+      points = IO_Points<float>::readCSV(string("data/sample_data.csv"));
+   }
    //KDTree<float> kdtree;// = new KDTree<float>();
    //kdtree = KDTree<float>::build_kdtree(points);
    auto kdtree = KDTree<float>::make_kdtree(points);    
